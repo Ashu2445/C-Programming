cf4eee85a9bd52977efef7fa5870d8bdebdc8195
Ashu2445 <ashuarya4444@gmail.com>
2025-08-13 12:10:37 +0530

    Add files via upload

---
+++ Program2Ashu.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+  int num1,num2,sub;
+  printf("enter two numbers\n");
+  scanf("%d %d",&num1,&num2);
+  sub=num1-num2;
+  printf("subtraction of %dand%d=%d\n",num1,num2,sub);
+  return 0;
+}
